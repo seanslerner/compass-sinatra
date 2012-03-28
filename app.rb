361a9083d1d6d6f23c5750157ac233c42c08a79d
Sean Lerner <seanslerner@gmail.com>
2012-03-28 09:11:38 -0700

    :public is no longer used to avoid overloading Module#public, use :public_folder instead

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'haml'    #must be loaded after sinatra
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :views, "views"
-set :public, 'static'
+set :public_folder, 'static'
 
 configure do
   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
